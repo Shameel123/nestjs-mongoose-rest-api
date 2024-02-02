77a5a9b75175cbe94c6b97f9331e2826bf245c2e
Shameel Uddin <shameel_uddin@yahoo.com>
2024-02-02 18:11:21 +0500

    Mongoose Setup Complete.

---
+++ src/user/user.service.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
+// import { Model } from 'mongoose';
+// import { InjectModel } from '@nestjs/mongoose';
+// import { User, UserDocument } from './schemas/user.schema';
+
+@Injectable()
+export class UserService {
+  // constructor(@InjectModel(User.name) private userModel: Model<UserDocument>) {}
+
+  create(createUserDto: CreateUserDto) {
+    // const createdUser = new this.userModel(createUserDto);
+    // return createdUser.save();
+    return 'This action creates user.';
+  }
+
+  findAll() {
+    return `This action returns all user`;
+  }
+
+  findOne(id: number) {
+    return `This action returns a #${id} user`;
+  }
+
+  update(id: number, updateUserDto: UpdateUserDto) {
+    return `This action updates a #${id} user`;
+  }
+
+  remove(id: number) {
+    return `This action removes a #${id} user`;
+  }
+}
