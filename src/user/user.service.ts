42b526247c66c42d456f6e07d0d41fd0da32c60f
Shameel Uddin <shameel_uddin@yahoo.com>
2024-02-02 18:57:57 +0500

    createUser Implemented with DTO validation.

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,18 +1,17 @@
 import { Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
-// import { Model } from 'mongoose';
-// import { InjectModel } from '@nestjs/mongoose';
-// import { User, UserDocument } from './schemas/user.schema';
+import { Model } from 'mongoose';
+import { InjectModel } from '@nestjs/mongoose';
+import { User, UserDocument } from './schemas/user.schema';
 
 @Injectable()
 export class UserService {
-  // constructor(@InjectModel(User.name) private userModel: Model<UserDocument>) {}
+  constructor(@InjectModel(User.name) private userModel: Model<UserDocument>) {}
 
   create(createUserDto: CreateUserDto) {
-    // const createdUser = new this.userModel(createUserDto);
-    // return createdUser.save();
-    return 'This action creates user.';
+    const createdUser = new this.userModel(createUserDto);
+    return createdUser.save();
   }
 
   findAll() {
