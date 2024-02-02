0ea05cd2e8c09724652ea03ecb0ee48772a83ceb
Shameel Uddin <shameel_uddin@yahoo.com>
2024-02-02 20:31:41 +0500

    CRUD Operations for User

--- src/user/dto/update-user.dto.ts
+++ src/user/dto/update-user.dto.ts
@@ -1,4 +1,17 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateUserDto } from './create-user.dto';
+import { IsEmail, IsNotEmpty, IsString, MinLength } from 'class-validator';
 
-export class UpdateUserDto extends PartialType(CreateUserDto) {}
+export class UpdateUserDto extends PartialType(CreateUserDto) {
+  @IsString()
+  @IsNotEmpty()
+  name: string;
+
+  @IsEmail()
+  email: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(6)
+  password: string;
+}
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { Model } from 'mongoose';
@@ -9,24 +9,51 @@ import { User, UserDocument } from './schemas/user.schema';
 export class UserService {
   constructor(@InjectModel(User.name) private userModel: Model<UserDocument>) {}
 
-  create(createUserDto: CreateUserDto) {
+  async create(createUserDto: CreateUserDto) {
+    if (await this.userModel.exists({ email: createUserDto.email })) {
+      throw new HttpException('User already exists', 400);
+    }
+
     const createdUser = new this.userModel(createUserDto);
     return createdUser.save();
   }
 
   findAll() {
-    return `This action returns all user`;
+    return this.userModel.find();
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} user`;
+  async findOne(id: string) {
+    const userExists = await this.userModel.exists({ _id: id });
+    if (!userExists) {
+      throw new HttpException('User not found', 404);
+    }
+    return this.userModel.findById(id);
   }
 
-  update(id: number, updateUserDto: UpdateUserDto) {
-    return `This action updates a #${id} user`;
+  async update(id: string, updateUserDto: UpdateUserDto) {
+    const userExists = await this.userModel.exists({ _id: id });
+    if (!userExists) {
+      throw new HttpException('User not found', 404);
+    }
+    const updatedUser = await this.userModel.findByIdAndUpdate(
+      id,
+      updateUserDto,
+      { new: true },
+    );
+    return updatedUser;
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} user`;
+  async remove(id: string) {
+    const userExists = await this.userModel.exists({ _id: id });
+    if (!userExists) {
+      throw new HttpException('User not found', 404);
+    }
+    return this.userModel.deleteOne({ _id: id });
+  }
+
+  async findByEmail(email: string) {
+    return this.userModel.findOne({
+      email,
+    });
   }
 }
