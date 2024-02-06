3e70c002e81b3d65e9c7c18d67b5d9d8842a4e36
Shameel Uddin <shameel_uddin@yahoo.com>
2024-02-06 23:33:16 +0500

    video 6

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -12,6 +12,7 @@ export class UserService {
   create(createUserDto: CreateUserDto) {
     const createdUser = new this.userModel(createUserDto);
     return createdUser.save();
+    // return this.userModel.create(createUserDto);
   }
 
   findAll() {
