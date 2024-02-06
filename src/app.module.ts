0fe45409dea15ed2e84098220251731baa20500a
Shameel Uddin <shameel_uddin@yahoo.com>
2024-02-06 19:27:20 +0500

    video 4

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,13 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [MongooseModule.forRoot('mongodb://localhost/nestjs-mongoose-rest')],
+  imports: [
+    MongooseModule.forRoot('mongodb://localhost/nestjs-mongoose-rest'),
+    UserModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
